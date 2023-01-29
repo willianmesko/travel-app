ac7e8739bdde401aed59c17185f222320c4b94d9
wmesko <willianmesko@hotmail.com>
2023-01-29 11:03:58 -0300

    create Accomodation anemic entitie

---
+++ src/domain/accomodation/validator/accomodation.yup.validator.ts
@@ -0,0 +1,42 @@
+import ValidatorInterface from "../../@shared/validator/validator.interface";
+import * as yup from "yup";
+import Accomodation from "../entity/accomodation";
+
+export default class AccomodationYupValidator
+  implements ValidatorInterface<Accomodation>
+{
+  validate(entity: Accomodation): void {
+    try {
+      yup
+        .object()
+        .shape({
+          id: yup.string().required("Id is required"),
+          title: yup.string().required("Title is required"),
+          start_date: yup.date().required("Start date is required"),
+          end_date: yup.date().required("End date is required"),
+          address: yup.string().notRequired(),
+          map: yup.string().notRequired(),
+          reference_url: yup.string().notRequired(),
+        })
+        .validateSync(
+          {
+            id: entity.id,
+            title: entity.title,
+            start_date: entity.start_date,
+            end_date: entity.end_date,
+          },
+          {
+            abortEarly: false,
+          }
+        );
+    } catch (errors) {
+      const e = errors as yup.ValidationError;
+      e.errors.forEach((error) => {
+        entity.notification.addError({
+          context: "Accomodation",
+          message: error,
+        });
+      });
+    }
+  }
+}
